5e2da5187e03ce6911fb3e34373fc65b1cd3be43
clemencebodereau <clemencebodereau@gmail.com>
2023-04-19 01:38:09 +0200

    Third commit

--- assets/footer.php
+++ assets/footer.php
@@ -1,30 +1,29 @@
 
 <footer>
 
-    <section>
+    <section class="footerformulaire">
         <h2>Contact</h2>
                 <p>Vous avez un projet ?</p>
-                <p>Je serai ravi d'en <a href="mailto:clemencebodereau@gmail.com">discuter</a></p>
-                <p>avec vous</p>
+                <p>Je serai ravi d'en discuter avec vous</p>
 
         <form action="/ma-page-de-traitement" method="post">
             <div>
-                <label for="name">Nom :</label>
-                <input type="text" id="name" name="user_name">
+                <label for="name">Nom et Prénom:</label>
+                <input type="text" id="name" name="user_name" placeholder="Votre nom et prénom">
             </div>
             <div>
-                <label for="name">Prénom :</label>
-                <input type="text" id="name" name="user_name">
+                <label for="name">Sujet:</label>
+                <input type="text" id="sujet" name="sujet"  placeholder="L'objet de votre message">
             </div>
             <div>
-                <label for="mail">e-mail&nbsp;:</label>
-                <input type="email" id="mail" name="user_mail">
+                <label for="mail">e-mail</label>
+                <input type="email" id="mail" name="email"  placeholder="Votre email">
             </div>
             <div>
                 <label for="msg">Message :</label>
-                <textarea id="msg" name="user_message"></textarea>
+                <textarea id="msg" name="message" placeholder="Votre message"></textarea>
             </div>
-            <button class="submit">Envoyer</button>
+            <button type="submit">Envoyer</button>
         </form>
 
     </section>
